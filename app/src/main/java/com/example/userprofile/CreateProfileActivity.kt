6883d74cf304a1e0cb61205c65c56e47a65ed74a
Winningtopas <jay07@live.nl>
2020-03-18 11:13:28 +0100

    Step 3

---
+++ app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
@@ -0,0 +1,69 @@
+package com.example.userprofile
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create_profile.*
+
+const val GALLERY_REQUEST_CODE = 100
+
+class CreateProfileActivity : AppCompatActivity() {
+
+    private var profileImageUri: Uri? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+        initViews()
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                GALLERY_REQUEST_CODE -> {
+                    profileImageUri = data?.data
+                    profile_image.setImageURI(profileImageUri)
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+    private fun initViews() {
+        open_picture_gallery.setOnClickListener { onGalleryClick() }
+        confirm.setOnClickListener { onConfirmClick() }
+    }
+
+    private fun onGalleryClick() {
+        // Create an Intent with action as ACTION_PICK
+        val galleryIntent = Intent(Intent.ACTION_PICK)
+
+        // Sets the type as image/*. This ensures only components of type image are selected
+        galleryIntent.type = "image/*"
+
+        // Start the activity using the gallery intent
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    private fun onConfirmClick() {
+        val profile = Profile(
+            first_name.toString(),
+            last_name.toString(),
+            description.toString(),
+            profileImageUri
+        )
+
+        val profileActivityIntent = Intent(this, ProfileActivity::class.java)
+        profileActivityIntent.putExtra(ProfileActivity.PROFILE_EXTRA, profile)
+        startActivity(profileActivityIntent)
+    }
+
+
+
+
+}
